1e2ea6aff0844bd7b4568f10226c4d5a024801a9
Anne Ensign <anne.ensign@gmail.com>
2021-09-08 14:26:32 -0400

    Code clean up and new append word function

--- scrabble.py
+++ scrabble.py
@@ -1,12 +1,29 @@
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+letters = [
+    "A", "B", "C", "D", "E", 
+    "F", "G", "H", "I", "J", 
+    "K", "L", "M", "N", "O", 
+    "P", "Q", "R", "S", "T", 
+    "U", "V", "W", "X", "Y", 
+    "Z"
+    ]
 
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+points = [
+    1, 3, 3, 2, 1, 
+    4, 2, 4, 1, 8, 
+    5, 1, 3, 4, 1, 
+    3, 10, 1, 1, 1, 
+    1, 4, 4, 8, 4, 
+    10
+    ]
+
 
 letter_to_points = {
     key: value
     for key, value
     in zip(letters, points)
 }
+
 
 letter_to_points[" "] = 0
 
@@ -28,23 +45,7 @@ player_to_words = {
 
 player_to_points = {}
 
-
-#  Iterate through the items in player_to_words. 
-# Call each player player and each list of words words.
-# Within your loop, create a variable called 
-# player_points and set it to 0.
-
-# Within the loop, create another loop 
-# that goes through each word in words 
-# and adds the value of score_word() 
-# with word as an input.
-
-# After the inner loop ends, 
-# set the current player value 
-# to be a key of player_to_points, 
-# with a value of player_points
-
-
+# ADD ALL PLAYER POINTS
 for player, words in player_to_words.items():
     player_points = 0
     for word in words:
@@ -52,4 +53,12 @@ for player, words in player_to_words.items():
     player_to_points[player] = player_points
 
 
-print(player_to_points)
+# ADD NEW WORD TO PLAYER'S WORD LIST
+def play_word(player, word):
+    player_to_words[player].append(word)
+
+
+play_word("player1", "CODE")
+
+
+print(player_to_words)
