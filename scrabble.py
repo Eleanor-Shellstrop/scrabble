faf07e96fcfe27f6b5f46fde8e3f78cfd64251d3
Anne Ensign <anne.ensign@gmail.com>
2021-09-08 11:41:38 -0400

    Add blank tile to dictionary

--- scrabble.py
+++ scrabble.py
@@ -2,18 +2,16 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
-# We have provided you with two lists, letters and points. 
-# We would like to combine these two into a dictionary 
-# that would map a letter to its point value.
-# Using a list comprehension and zip, 
-# create a dictionary called letter_to_points 
-# that has the elements of letters as the keys 
-# and the elements of points as the values.
-
 letter_to_points = {
     key: value
     for key, value
     in zip(letters, points)
 }
+
+# Our letters list did not take into account blank tiles. 
+# Add an element to the letter_to_points dictionary 
+# that has a key of " " and a point value of 0.
+
+letter_to_points[" "] = 0
 
 print(letter_to_points)
